c7dfdc374fba3835ad27f5a055c0ad23fab5f1f7
coddy33 <nanouwow@hotmail.fr>
2019-01-30 11:57:46 +0100

    nex color gradient function

--- projet.py
+++ projet.py
@@ -7,12 +7,9 @@ JUNG Frédéric
 
 Visual analysis of gene expression data
 
-01/26/2016
-"""
+01/26/2019
 
-from tulip import tlp
 
-"""
 MIT License
 
 Copyright (c) 2019 JUNG Frédéric | BLAIS Benjamin
@@ -35,6 +32,8 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+
+from tulip import tlp
 
 
 def preprocessing(gr, viewColor):
@@ -132,7 +131,8 @@ def compute_path(L1, L2):
   Find the shortest path between two nodes using two lists. The lists are the path between the node of interest and the root node.
   The first common node between this two path is the closest node which links the source and the target. 
   
-  This function allow to find the first common node and remove the others, then return the path between the source and the target.
+  This function allow to find the first common node between two lists and remove the others nodes.
+  Then return the path between the source and the target.
   
   @type  L1: list
   @param L1: first path between the source node and root.
@@ -177,12 +177,24 @@ def set_control_points(tree, gene, path, e):
   # position_vector sous cette forme : [(-2683.61,3082.42,0), (-1554.76,2237.48,0), (-1155.87,721.011,0)]  
   gene.setEdgeValue(e, position_vector)
 
-def find_parents(gr, node, position):
+def find_parents(gr, node, parents):
+  """
+  Find the shortest path between the node and its parent.
+  
+  @type       gr: tlp.Graph
+  @param      gr: the current graph
+  @type     node: tlp.node
+  @param    node: the node of intereset
+  @type  parents: list
+  @param parents: empty list of node'parents
+  @rtype        : list
+  @return       : list of parents
+  """
   if gr.getInNodes(node) > 0 :
-    position.append(node)
+    parents.append(node)
     for n in gr.getInNodes(node):
-      find_path2(gr, n, position)
-  return position
+      find_parents(gr, n, parents)
+  return parents
 
 def find_path(gr, source, target):
   L1 = []
@@ -212,18 +224,40 @@ def draw_bundles(gene, tree):
     set_control_points(viewLayout_hierarchical, viewLayout_interraction, path, e)
   viewShape.setAllEdgeValue(tlp.EdgeShape.CubicBSplineCurve)
 
-
+def color_gradient(nb_gradient):
+  colors = []
+  r = 255
+  g = 0
+  b = 0
+  gradient = 255/(nb_gradient/2)
+  for i in range(nb_gradient/2) :
+    colors.append(tlp.Color(r,g,b))
+    r = r - gradient
+  r = 0
+  colors.append(tlp.Color.Black)
+  for i in range (nb_gradient/2):
+    colors.append(tlp.Color(r,g,b))
+    g = g + gradient
+  return colors
+  
 #colorier les sommets (couleur à regler)
 def color_graph(gr,param,color):
   """
   
   """
   params = tlp.getDefaultPluginParameters("Color Mapping",gr)
+  colorScale = tlp.ColorScale([])
   params["input property"] = param
-  params["minimum value"]=0
-  params["maximum value"]=15
-  #params["color scale"]=
+#  params["minimum value"]=0
+#  params["maximum value"]=15
+#  colors = [tlp.Color.Red, tlp.Color.Black, tlp.Color.Green]
+#  colors=[tlp.Color.Red,tlp.Color.Red,tlp.Color(192,0,0),tlp.Color(192,0,0),tlp.Color(128,0,0),tlp.Color(128,0,0),tlp.Color(64,0,0),tlp.Color(0,0,0),tlp.Color(0,0,0),tlp.Color(0,51,0),tlp.Color(0,51,0),tlp.Color(0,102,0),tlp.Color(0,153,0),tlp.Color(0,153,0),tlp.Color(0,204,0),tlp.Color(0,204,0),tlp.Color.Green,tlp.Color.Green,tlp.Color.Green]
+  colors = color_gradient(20)
+  colorScale.setColorScale(colors)
+  params["color scale"] = colorScale
   gr.applyColorAlgorithm("Color Mapping", color, params)
+
+
 
 
 def timePoint_hierarchy(nb_TP):
@@ -248,7 +282,9 @@ def timePoint_hierarchy(nb_TP):
   
 def draw_timePoint_hierarchy(TPs, SM, gene):
   """
+  Add a Small Multiples subgraph for each time points with the gene interractions properties.
   
+  Time points data are set in the viewMetric columns and defined the graph colors.
   
   @type   TPs: list 
   @param  TPs: the Time points name 
@@ -272,27 +308,17 @@ def main(graph):
   viewLayout = graph.getLayoutProperty("viewLayout")
   viewColor = graph.getColorProperty("viewColor")
   param = graph.getDoubleProperty("tp1 s")
-
-
   preprocessing(graph, viewColor)
-  
   root_cluster = graph.getSubGraph("Genes interactions")
   hierarchical_tree = graph.addSubGraph("hierarchical_graph") # init new graph 
-  
   root = hierarchical_tree.addNode({"name":"root"}) # first node as root 
   draw_hierarchical_tree(hierarchical_tree, root, root_cluster)
-  
   apply_radial_algorithm(hierarchical_tree,viewLayout)
-  
-
   draw_bundles( root_cluster, hierarchical_tree)
   color_graph(root_cluster,param,viewColor)
-  
   TPs = timePoint_hierarchy(17)
   SM = graph.addSubGraph("Small multiples")
   draw_timePoint_hierarchy(TPs, SM, root_cluster)
-
-
   updateVisualization(centerViews = True)
 
  
